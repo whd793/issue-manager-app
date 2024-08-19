ba60fbc4cb281ce32e725f9c166e66af8e29bd27
Won Lee <whd793@gmail.com>
2024-08-19 18:17:09 +0900

    disbla ssr for loading md editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { Button, Callout, Text, TextField } from '@radix-ui/themes';
-import SimpleMDE from 'react-simplemde-editor';
+import dynamic from 'next/dynamic';
 import { useForm, Controller } from 'react-hook-form';
 import axios from 'axios';
 import 'easymde/dist/easymde.min.css';
@@ -12,6 +12,10 @@ import { createIssueSchema } from '@/app/validationSchemas';
 import { z } from 'zod';
 import ErrorMessage from '@/app/components/ErrorMessage';
 import Spinner from '@/app/components/Spinner';
+
+const SimpleMDE = dynamic(() => import('react-simplemde-editor'), {
+  ssr: false,
+});
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
@@ -40,25 +44,22 @@ const NewIssuePage = () => {
   });
 
   return (
-    <div className="max-w-xl">
+    <div className='max-w-xl'>
       {error && (
-        <Callout.Root color="red" className="mb-5">
+        <Callout.Root color='red' className='mb-5'>
           <Callout.Text>{error}</Callout.Text>
         </Callout.Root>
       )}
-      <form
-        className="space-y-3"
-        onSubmit={onSubmit}
-      >
+      <form className='space-y-3' onSubmit={onSubmit}>
         <TextField.Root>
-          <TextField.Input placeholder="Title" {...register('title')} />
+          <TextField.Input placeholder='Title' {...register('title')} />
         </TextField.Root>
         <ErrorMessage>{errors.title?.message}</ErrorMessage>
         <Controller
-          name="description"
+          name='description'
           control={control}
           render={({ field }) => (
-            <SimpleMDE placeholder="Description" {...field} />
+            <SimpleMDE placeholder='Description' {...field} />
           )}
         />
         <ErrorMessage>{errors.description?.message}</ErrorMessage>
