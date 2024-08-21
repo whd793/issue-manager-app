9596329cbdd660e2d0421ddedc0cae0c7281a797
Won Lee <whd793@gmail.com>
2024-08-21 12:08:03 +0900

    fix filtering issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -54,7 +54,10 @@ const IssuesPage = async ({ searchParams }: Props) => {
         <Table.Header>
           <Table.Row>
             {columns.map((column) => (
-              <Table.ColumnHeaderCell key={column.value}>
+              <Table.ColumnHeaderCell
+                key={column.value}
+                className={column.className}
+              >
                 <NextLink
                   href={{
                     query: { ...searchParams, orderBy: column.value },
