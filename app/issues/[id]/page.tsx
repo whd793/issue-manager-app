f1eb33f13a3dff3fca2767134b579f268efaf71a
Won Lee <whd793@gmail.com>
2024-08-22 02:30:11 +0900

    Add metadata

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -22,13 +22,13 @@ const IssueDetailPage = async ({ params }: Props) => {
   if (!issue) notFound();
 
   return (
-    <Grid columns={{ initial: '1', sm: '5' }} gap='5'>
-      <Box className='md:col-span-4'>
+    <Grid columns={{ initial: '1', sm: '5' }} gap="5">
+      <Box className="md:col-span-4">
         <IssueDetails issue={issue} />
       </Box>
       {session && (
         <Box>
-          <Flex direction='column' gap='4'>
+          <Flex direction="column" gap="4">
             <AssigneeSelect issue={issue} />
             <EditIssueButton issueId={issue.id} />
             <DeleteIssueButton issueId={issue.id} />
@@ -38,5 +38,14 @@ const IssueDetailPage = async ({ params }: Props) => {
     </Grid>
   );
 };
+
+export async function generateMetadata({ params }: Props) {
+  const issue = await prisma.issue.findUnique({ where: { id: parseInt(params.id) }});
+
+  return {
+    title: issue?.title,
+    description: 'Details of issue ' + issue?.id
+  }
+}
 
 export default IssueDetailPage;
