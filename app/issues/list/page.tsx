57d6c9bdce5d06ed9b8ec8e42fa5c10cef756c39
Won Lee <whd793@gmail.com>
2024-08-21 12:06:20 +0900

    fix sorting issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -1,13 +1,13 @@
-import prisma from "@/prisma/client";
-import { Table } from "@radix-ui/themes";
-import { IssueStatusBadge, Link } from "@/app/components";
+import prisma from '@/prisma/client';
+import { Table } from '@radix-ui/themes';
+import { IssueStatusBadge, Link } from '@/app/components';
 import NextLink from 'next/link';
-import IssueActions from "./IssueActions";
-import { Issue, Status } from "@prisma/client";
-import { ArrowUpIcon } from "@radix-ui/react-icons";
+import IssueActions from './IssueActions';
+import { Issue, Status } from '@prisma/client';
+import { ArrowUpIcon } from '@radix-ui/react-icons';
 
 interface Props {
-  searchParams: { status: Status, orderBy: keyof Issue };
+  searchParams: { status: Status; orderBy: keyof Issue };
 }
 
 const IssuesPage = async ({ searchParams }: Props) => {
@@ -16,16 +16,16 @@ const IssuesPage = async ({ searchParams }: Props) => {
     value: keyof Issue;
     className?: string;
   }[] = [
-    { label: "Issue", value: "title" },
+    { label: 'Issue', value: 'title' },
     {
-      label: "Status",
-      value: "status",
-      className: "hidden md:table-cell",
+      label: 'Status',
+      value: 'status',
+      className: 'hidden md:table-cell',
     },
     {
-      label: "Created",
-      value: "createdAt",
-      className: "hidden md:table-cell",
+      label: 'Created',
+      value: 'createdAt',
+      className: 'hidden md:table-cell',
     },
   ];
 
@@ -34,24 +34,37 @@ const IssuesPage = async ({ searchParams }: Props) => {
     ? searchParams.status
     : undefined;
 
+  const orderBy = columns
+    .map((column) => column.value)
+    .includes(searchParams.orderBy)
+    ? { [searchParams.orderBy]: 'asc' }
+    : undefined;
+
   const issues = await prisma.issue.findMany({
     where: {
       status,
     },
+    orderBy,
   });
 
   return (
     <div>
       <IssueActions />
-      <Table.Root variant="surface">
+      <Table.Root variant='surface'>
         <Table.Header>
           <Table.Row>
             {columns.map((column) => (
               <Table.ColumnHeaderCell key={column.value}>
-                <NextLink href={{
-                  query: { ...searchParams, orderBy: column.value }                  
-                }}>{column.label}</NextLink>
-                {column.value === searchParams.orderBy && <ArrowUpIcon className="inline"/>}
+                <NextLink
+                  href={{
+                    query: { ...searchParams, orderBy: column.value },
+                  }}
+                >
+                  {column.label}
+                </NextLink>
+                {column.value === searchParams.orderBy && (
+                  <ArrowUpIcon className='inline' />
+                )}
               </Table.ColumnHeaderCell>
             ))}
           </Table.Row>
@@ -60,17 +73,15 @@ const IssuesPage = async ({ searchParams }: Props) => {
           {issues.map((issue) => (
             <Table.Row key={issue.id}>
               <Table.Cell>
-                <Link href={`/issues/${issue.id}`}>
-                  {issue.title}
-                </Link>
-                <div className="block md:hidden">
+                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
+                <div className='block md:hidden'>
                   <IssueStatusBadge status={issue.status} />
                 </div>
               </Table.Cell>
-              <Table.Cell className="hidden md:table-cell">
+              <Table.Cell className='hidden md:table-cell'>
                 <IssueStatusBadge status={issue.status} />
               </Table.Cell>
-              <Table.Cell className="hidden md:table-cell">
+              <Table.Cell className='hidden md:table-cell'>
                 {issue.createdAt.toDateString()}
               </Table.Cell>
             </Table.Row>
@@ -81,6 +92,6 @@ const IssuesPage = async ({ searchParams }: Props) => {
   );
 };
 
-export const dynamic = "force-dynamic";
+export const dynamic = 'force-dynamic';
 
 export default IssuesPage;
